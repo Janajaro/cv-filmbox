9bd987ba0a26726e9957e833fbc2118c021018f3
Jana Jarošová <Janamjaros@seznam.cz>
2024-04-11 19:09:57 +0200

    druhe cviko

--- spolecne.js
+++ spolecne.js
@@ -3,13 +3,12 @@ const btnMenu = document.querySelector('#menu-tlacitko');
 btnMenu.addEventListener('click', (event) => {
   const btnItems = document.querySelector('#menu-polozky');
   btnItems.classList.toggle('show');
+  const btnElm = document.querySelector('i');
+  if (btnElm.classList.contains('fa-bars')) {
+    btnElm.classList.remove('fa-bars');
+    btnElm.classList.add('fa-xmark');
+  } else {
+    btnElm.classList.add('fa-bars');
+    btnElm.classList.remove('fa-xmark');
+  }
 });
-
-/* pokusy:)
-let symbol;
-if (symbol === document.querySelector('.fas fa-xmark')) {
-  btnMenu.classList.add('fas fa-bars');
-} else {
-  btnMenu.classList.add('fas fa-xmarks');
-}
-*/
